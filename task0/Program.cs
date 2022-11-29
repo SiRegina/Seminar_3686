718b166aa0ac9f610641470c4d8b538d4d294a26
Regina <regina-22.87@mail.ru>
2022-11-30 00:39:11 +0500

    Преобразование текста в число

---
+++ task0/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число: ");
+
+// string answer = Console.ReadLine(); //"123"
+// int number = Convert.ToInt32(answer); //"123" ->123
+
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(number*number);
