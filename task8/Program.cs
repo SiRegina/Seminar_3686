b085a1cb6c95c48824608be06e425f4ce3977b02
Regina <regina-22.87@mail.ru>
2022-11-30 19:08:00 +0500

    Показать все чётные числа от 1 до N

---
+++ task8/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());  
+
+int number=1;
+
+while (number<=N)
+{
+    int i = number % 2;
+    if(i == 0)
+    {
+        Console.Write(number+", ");
+    }
+        number++;
+    
+}
+
