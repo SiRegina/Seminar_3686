a32a009f05d3c591e610632f496a571cb0afc7dc
Regina <regina-22.87@mail.ru>
2022-12-06 00:05:37 +0500

    010 Показывает вторую цифру трехзначного числа

--- task10/Program.cs
+++ task10/Program.cs
@@ -8,10 +8,8 @@
 
 int number = new Random().Next(100,1000);
 
-int digit1 = number/100;
-int digit3 = number % 10;
-
-int rez = (number - digit1*100 - digit3) / 10;    
+int digit1 = number/10;
+int rez = digit1 % 10;
 
 Console.WriteLine($"Случайное число: {number}");
 Console.WriteLine($"Итоговое число: {rez}");
