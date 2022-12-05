ad2cfe70d80ccf1c07013317421de7b49576d8cd
Regina <regina-22.87@mail.ru>
2022-12-05 22:32:36 +0500

    012 Проверка кратности числа одновременно 7 и 23

---
+++ task14/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 14: Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+
+Console.Write("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+if(numberA%7 == 0 & numberA%23 ==0)
+   {
+        Console.WriteLine($"{numberA} делится на 7 и 23");
+    }
+else
+    {
+        Console.WriteLine($"{numberA}  не делится одновременно на 7 и 23 ");
+    }
+
+
+    
