a397be3a221ba09778646d54561147b8814ac515
Regina <regina-22.87@mail.ru>
2022-12-06 22:57:24 +0500

    018 показывает диапазон возможных координат точек в этой четверти (x и y)

---
+++ task18/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 18: Напишите программу, которая по заданному номеру четверти, 
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.WriteLine("Введите номер четверти от 1 до 4");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == 1)
+{
+    Console.WriteLine("X>0 и y>0"); 
+}
+
+if (number == 2)
+{
+    Console.WriteLine("X<0 и y>0");
+}
+
+if (number ==3)
+{
+    Console.WriteLine("X<0 и y<0");
+}
+
+if (number ==4)
+{
+    Console.WriteLine("X>0 и y<0");
+}
+
+if (number>4 || number <1)
+{
+    Console.WriteLine("Четвертей только четыре, введите значение от 1 до 4");
+}
