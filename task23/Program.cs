f861f0bb44bbd6472ba644da30d7d1b867cbd3fe
Regina <regina-22.87@mail.ru>
2022-12-07 15:34:03 +0500

    023 принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N

---
+++ task23/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число N: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+int n=1;
+
+for (n = 1; n<= number; n++)
+{
+    Console.Write ( n*n*n+", ");
+    
+}
+Console.WriteLine ("Таблица кубов чисел от 1 до " + number);
