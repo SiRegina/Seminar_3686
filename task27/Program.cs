96fa78a044a063380b941f0f70e27788b1b5f748
Regina <regina-22.87@mail.ru>
2022-12-11 23:46:12 +0500

    027 принимает на вход число и выдаёт сумму цифр в числе

---
+++ task27/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetSumOfNumber(int number)
+{
+    int sum = 0;
+    while (number!=0)
+    {
+        int n =number;  
+        n = n % 10;
+        sum = sum + n;
+        number/=10;
+    }
+    return sum;
+}
+
+int A=ReadInt("Введите число: ");
+
+int result = GetSumOfNumber(A);
+Console.WriteLine ($"Сумма цифр в числе {A} = {result}");
