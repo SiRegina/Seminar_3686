2948688d3bd23a664583957e165d19331fc56952
Regina <regina-22.87@mail.ru>
2022-12-12 00:27:25 +0500

    029 задаёт массив из 8 элементов и выводит их на экран. Данные вводятся с консоли пользователем

---
+++ task29/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. Данные вводятся с консоли пользователем
+
+
+int[] array = new int [8]; 
+
+void EnterArray(int [] array)
+{
+    for (int i = 0; i<8; i++)
+    {
+        Console.Write("Введите " + (i+1) + " число: " );
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+        
+EnterArray(array);
+
+Console.WriteLine(string.Join(", ",array));
+
