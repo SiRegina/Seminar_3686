b625011c4c36b759ebe3bdd1e610024277900682
Regina <regina-22.87@mail.ru>
2022-12-11 22:36:21 +0500

    025 принимает на вход два числа (A и B) и возводит число A в натуральную степень B

---
+++ task25/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int Exponentiates(int number, int pow)
+{
+    int res=1;
+    for (int i = 0; i<pow; i++)
+    {
+        res=res*number;
+    }
+    return res;
+}
+
+int A=ReadInt("Введите число: ");
+int B=ReadInt("Введите степень, в которую будет возводиться число: ");
+
+
+int result = Exponentiates(A,B);
+Console.WriteLine ($"Число {A} в степени {B} = {result}");
