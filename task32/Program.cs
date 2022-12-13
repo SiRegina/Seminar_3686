592a369d2a18dd7de21382c97071658315e4f376
Regina <regina-22.87@mail.ru>
2022-12-13 23:12:27 +0500

    032 замена элементов массива: положительные элементы замените на соответствующие отрицательные

---
+++ task32/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int[] GetRandomArray(int length, int leftRange, int rightRange)
+{
+    int[] array = new int[length]; 
+    Random rand = new Random();
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(leftRange, rightRange + 1);
+    }
+
+    return array;
+}
+
+int [] GetOppositeArray(int[] array)
+{
+    int[] array1 = new int[array.Length];
+
+
+    for(int j = 0; j < array.Length; j++)
+    {
+        array1[j] = array[j]*(-1);
+    }
+
+    return array1;
+}
+const int LENGTH = 4;
+const int LEFTRANGE = -9;
+const int RIGHTRANGE = 9;  
+int[] massive = GetRandomArray(LENGTH, LEFTRANGE, RIGHTRANGE);
+Console.WriteLine($"{string.Join(", ", massive)}");
+int[] massive2 = GetOppositeArray(massive);
+Console.WriteLine($"{string.Join(", ", massive2)}");
