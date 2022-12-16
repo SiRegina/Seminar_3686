339c683693649ca12160a90bb528dfc63f00fe53
Regina <regina-22.87@mail.ru>
2022-12-17 00:18:11 +0500

    042 преобразовывать десятичное число в двоичное

---
+++ task42/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+void PrintBinaryViev(int n)
+{
+    if(n<=0) return;
+    PrintBinaryViev(n/2);
+    Console.Write(n%2);
+}
+
+
+int number=ReadInt("Введите число: ");
+
+PrintBinaryViev(number);
