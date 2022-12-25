457550e1a08efccdef078d40b03b607237eaf49c
Regina <regina-22.87@mail.ru>
2022-12-26 00:01:49 +0500

    69 возводит число А в целую степень B с помощью рекурсии

---
+++ task69/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 69: Напишите программу, которая на вход принимает два числа A и B, 
+//и возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int PowNumber (int A, int B)
+{
+    if (B==0)
+    {
+        return 1;
+    }
+    return A*PowNumber(A,B-1);
+    
+}
+
+int A = ReadInt("Введите число A: ");
+int B = ReadInt("Введите число B: ");
+System.Console.WriteLine(PowNumber(A,B));
