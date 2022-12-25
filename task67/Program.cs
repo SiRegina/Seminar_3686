267e6a012ce5de3f3b84e7bd813b579439a350a6
Regina <regina-22.87@mail.ru>
2022-12-26 00:02:58 +0500

    67 принимать на вход число и возвращать сумму его цифр

---
+++ task67/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+
+
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumNumber (int N)
+{
+    if (N==0)
+    {
+        return 0;
+    }
+
+    return N%10+SumNumber(N/10);
+}
+
+int N = ReadInt("Введите число: ");
+System.Console.WriteLine(SumNumber(N));
