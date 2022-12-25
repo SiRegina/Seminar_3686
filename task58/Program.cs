caac3f797b55c5c7687dbb18f7552692f8689ad8
Regina <regina-22.87@mail.ru>
2022-12-26 01:48:55 +0500

    58 находить произведение двух матриц

---
+++ task58/Program.cs
@@ -0,0 +1,79 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, заданы 2 массива:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// и
+// 1 5 8 5
+// 4 9 4 2
+// 7 2 2 6
+// 2 3 4 7
+// Их произведение будет равно следующему массиву:
+// 1 20 56 10
+// 20 81 8 6
+// 56 8 4 24
+// 10 6 24 49
+
+
+
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GetRandomMatrix(int rows, int columns, int leftRange = 0, int rightRange = 10)
+{
+    int[,] matr = new int[rows, columns];
+
+    var rand = new Random();
+    for(int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+
+    return matr;
+}
+
+void PrintMatrix(int[,] matr)
+{
+    for(int i = 0; i < matr.GetLength(0); i++)
+    {
+        Console.WriteLine();
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write(matr[i, j] + " ");
+        }
+    }
+}
+
+int[,] ProizvedMatrix(int[,] matr1, int[,] matr2)   
+{
+    int[,] matrix= new int[matr1.GetLength(0), matr1.GetLength(1)];
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        { 
+            matrix[i,j] = matr1[i,j]*matr2[i,j];
+        }
+        
+    }
+    return matrix;
+}
+
+int rowsCount = ReadInt("Введите число строк");
+int columnsCount = ReadInt("Введите число столбцов");
+int[,] matrix1 = GetRandomMatrix(rowsCount, columnsCount);
+PrintMatrix(matrix1);
+System.Console.WriteLine();
+int[,] matrix2 = GetRandomMatrix(rowsCount, columnsCount);
+PrintMatrix(matrix2);
+System.Console.WriteLine();
+
+int[,] matrix = ProizvedMatrix(matrix1, matrix2);
+PrintMatrix(matrix);
+
